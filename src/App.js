e12643929b5fc2494f01ef85faf27460443916f9
alhbib699 <alhbib699@gmail.com>
2026-04-26 17:10:01 +0300

    create StartScreen component and integrate it into app flow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Headers from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -18,8 +21,7 @@ function reducer(state, action) {
         status: "ready",
       };
     case "dataFailed":
-      return{...state,status:"error"}
-
+      return { ...state, status: "error" };
 
     default:
       throw new Error("Action unknown");
@@ -27,13 +29,15 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({type:"dataFailed" }));
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
@@ -41,8 +45,9 @@ function App() {
       <Headers />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions}/>}
       </Main>
     </div>
   );
