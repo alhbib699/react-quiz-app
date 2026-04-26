c0736036a7341b8827c24677714fffc8cd67a919
alhbib699 <alhbib699@gmail.com>
2026-04-26 16:19:04 +0300

    implement state management using useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import Headers from "./Header"
+import { useEffect, useReducer } from "react";
+import Headers from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  // ready, loading, finished, active, error
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return{...state,status:"error"}
+
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({type:"dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
-      <Headers/>
+      <Headers />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
