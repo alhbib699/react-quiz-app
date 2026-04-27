a97e3344b8b32905867d6cbda5cc16d75ae10668
alhbib699 <alhbib699@gmail.com>
2026-04-27 23:07:31 +0300

    implement quiz timer functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,6 +8,10 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION =30;
 
 const initialState = {
   questions: [],
@@ -17,7 +21,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
-  highScore:0,
+  highScore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -31,7 +36,7 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return { ...state, status: "active" , secondsRemaining: state.questions.length * SECS_PER_QUESTION };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
@@ -45,19 +50,25 @@ function reducer(state, action) {
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
     case "finish":
-      return {...state,   status: "finished" , highScore: state.points > state.highScore ? state.points : state.highScore}
-      case "restart":
-        return {...state, questions:state.questions , status:"ready"}
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+
+      case "tick":
+        return {...state, secondsRemaining:state.secondsRemaining - 1 , status: state.secondsRemaining === 0 ? "finished" : state.status}
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points ,highScore}, dispatch] = useReducer(
-    reducer,
-    initialState,
-  );
+  const [{ questions, status, index, answer, points, highScore , secondsRemaining}, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -96,12 +107,25 @@ function App() {
               answer={answer}
               dispatch={dispatch}
             />
-            <NextButton dispatch={dispatch} answer={answer} numQuestions={numQuestions} index={index}/>
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
+            </Footer>
           </>
         )}
 
         {status === "finished" && (
-          <FinishScreen maxPossiblePoints={maxPossiblePoints} points={points} highScore={highScore} dispatch={dispatch}/>
+          <FinishScreen
+            maxPossiblePoints={maxPossiblePoints}
+            points={points}
+            highScore={highScore}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
--- src/Components/Options.js
+++ src/Components/Options.js
@@ -3,14 +3,18 @@ function Options({ question, dispatch, answer }) {
   return (
     <div className="options">
       {question.options.map((option, index) => (
+      
+        
         <button
           className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswer ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
           key={option}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
           disabled={hasAnswer}
+          
         >
           {option}
         </button>
+        
       ))}
     </div>
   );
