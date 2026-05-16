ec3edf19280f714c86d78d86b162f7744bdf3948
alhbib699 <alhbib699@gmail.com>
2026-05-17 00:29:20 +0300

    refactor: replace prop drilling with Context API in quiz app

--- src/Components/StartScreen.js
+++ src/Components/StartScreen.js
@@ -1,8 +1,10 @@
-function StartScreen({ numQuestions, dispatch }) {
-  console.log(numQuestions)
+import { useQuiz } from "../contexts/QuizContext";
+
+function StartScreen() {
+  const { numQuestions, dispatch } = useQuiz();
   return (
     <div className="start">
-      <h2>Welcome to The React Quiz!</h2>
+      <h2>Welcome to The  Quiz!</h2>
       <h3>{numQuestions} questions to test your react mastery</h3>
       <button
         className="btn btn-ui"
